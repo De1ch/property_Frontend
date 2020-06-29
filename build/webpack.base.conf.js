354a556439e94235332e2d5c5aea62bfe245e5d1
Nikita Deich <54121126+De1ch@users.noreply.github.com>
2020-06-30 02:47:10 +0600

    Changing the project assembly template (assets & fonts path)

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -12,7 +12,7 @@ const { VueLoaderPlugin } = require("vue-loader");
 const PATHS = {
   src: path.join(__dirname, "../src"),
   dist: path.join(__dirname, "../dist"),
-  assets: "assets/"
+  assets: "./assets/"
 };
 
 const PAGES_DIR = `${PATHS.src}/pages`
@@ -31,7 +31,6 @@ module.exports = {
   output: {
     filename: `${PATHS.assets}js/[name].[contenthash].js`,
     path: PATHS.dist,
-    publicPath: "/"
   },
   optimization: {
     splitChunks: {
